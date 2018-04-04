7fa85c986ac3b72f0f8675e6a4ffd1d014e25c1d
mazminier <32083336+mazminier@users.noreply.github.com>
2018-04-03 20:54:17 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Twitter API Keys
+consumer_key = "EoCBWkWTFPxFrGC2FfGbmIyZu"
+consumer_secret = "BCFCPowJXHteiP4Z3yJ5RLJEzLfxXHke8Jc5Crk8GdfnTM2sSa"
+access_token = "905871634770391042-9uRcpOru3Ln7LgTiTiNxqynJFRnvDTA"
+access_token_secret = "2gBT7oItPmaISdA5NIzCRy0L6PQPwr9ROYE7U4tkDuyxO"
+
+# Open Weather Map
+api_key = 'f60742b2784a10349f691adcfe4c61a2'
